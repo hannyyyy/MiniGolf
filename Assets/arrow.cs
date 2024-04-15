feb247325254f5ced193bea878700befee4d4bbc
Hanny <s107413@ltisdschools.net>
2024-04-15 16:02:11 -0500

    day 2: working on arrow/line drawn on the ball

---
+++ Assets/arrow.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    [SerializeField] private float shotPower;
+    [SerializeField] private float stopVelocity = 0.05f;
+    [SerializeField] private LineRenderer lineRenderer;
+    private bool isIdle;
+    private bool isAiming;
+    private Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+
+        isAiming = false;
+        lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(body.velocity.magnitude < stopVelocity)
+        {
+            Stop(); 
+        }
+        ProcessAim();
+    }
+    private void OnMouseDown()
+    {
+        if (isIdle)
+        {
+            isAiming = true;
+        }
+    }
+    private void ProcessAim()
+    {
+        if(!isAiming || !isIdle )
+        {
+            return;
+        }
+        Vector3? worldPoint = CastMouseClickRay();
+
+        if(!worldPoint.HasValue)
+        {
+            return;
+        }
+        DrawLine(worldPoint.Value);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot(worldPoint.Value);
+        }
+    }
+    private void Shoot(Vector3 worldPoint)
+    {
+        isAiming = false;
+        lineRenderer.enabled = false;
+
+        Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
+
+        Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
+        float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
+
+        body.AddForce(direction * strength * shotPower);
+    }
+    private void DrawLine(Vector3 worldPoint)
+    {
+        Vector3[] positions = { transform.position, worldPoint };
+        lineRenderer.SetPositions(positions);
+        lineRenderer.enabled = true;
+    }
+    private void Stop()
+    {
+        body.velocity = Vector3.zero;
+        body.angularVelocity = Vector3.zero;
+        isIdle = false;
+    }
+
+    private Vector3? CastMouseClickRay()
+    {
+        Vector3 screenMousePosFar = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.farClipPlane);
+        Vector3 screenMousePosNear = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.nearClipPlane);
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+        RaycastHit hit;
+        if(Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity))
+        {
+            return hit.point;
+        } else
+        {
+            return null;
+        }
+
+    }
+}
