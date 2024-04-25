e63e0bfae33adf8f7d78c548e1f08d93569d2229
Hanny <s107413@ltisdschools.net>
2024-04-25 16:02:47 -0500

    Worked on the player movement and aiming the ball

--- Assets/lineForce.cs
+++ Assets/lineForce.cs
@@ -6,26 +6,31 @@ using UnityEngine.UI;
 public class lineForce : MonoBehaviour
 {
     [SerializeField] private float shotPower = 5f;
-    [SerializeField] private float stopVelocity = 0.05f;
+    [SerializeField] private float stopVelocity = 0.02f;
     [SerializeField] private LineRenderer lineRenderer;
-    private bool isIdle = true;
-    private bool isAiming = true;
+    private bool isIdle;
+    private bool isAiming;
+
     private Rigidbody body;
     public int clickCount;
     public Text s;
     private bool goal = false;
     public Canvas levelOver;
 
-    void Update()
+    void FixedUpdate()
     {
+        if (body.velocity.magnitude < stopVelocity)
+        {
+            Stop();
+        }
         ProcessAim();
     }
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
 
-        //isAiming = false;
-        //lineRenderer.enabled = false;
+        isAiming = false;
+        lineRenderer.enabled = false;
     }
     private void ProcessAim()
     {
@@ -54,17 +59,17 @@ public class lineForce : MonoBehaviour
             Shoot(worldPoint.Value);
         }
     }
-   /* private void OnMouseDown()
+   private void OnMouseDown()
     {
         if (isIdle)
         {
             isAiming = true;
         }
-    }*/
+    }
     private void Shoot(Vector3 worldPoint)
     {
-        //isAiming = false;
-        //lineRenderer.enabled = false;
+        isAiming = false;
+        lineRenderer.enabled = false;
 
         Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
 
@@ -72,12 +77,19 @@ public class lineForce : MonoBehaviour
         float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
 
         body.AddForce(direction * strength * shotPower);
+        isIdle = true;
     }
     private void DrawLine(Vector3 worldPoint)
     {
         Vector3[] positions = { transform.position, worldPoint };
         lineRenderer.SetPositions(positions);
         lineRenderer.enabled = true;
+    }
+    private void Stop()
+    {
+        body.velocity = Vector3.zero;
+        body.angularVelocity = Vector3.zero;
+        isIdle = true;
     }
 
     private Vector3? CastMouseClickRay()
